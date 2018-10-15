9ca510393af92901a7981b6bd8e008eea1da4df9
dngo <dodugaga@yahoo.com>
2018-10-14 23:16:18 -0400

    Error handling, ngx bootstrap and theme

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { AlertifyService } from './../_services/alertify.service';
+import { AuthService } from './../_services/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+
+  ngOnInit() {}
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('Successfully loged in');
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+  loggedIn() {
+    return this.authService.loggedIn();
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    this.alertify.message('Succesfully logged out');
+  }
+}
